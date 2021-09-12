ecb5ea4ec1a12979a5152bb7ae831d3c0c7fa73c
artemyunusov <artemyunusov@gmail.com>
2021-09-13 02:30:07 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+	const swiper = new Swiper('.swiper', {
+		speed: 2000,
+		autoplay: {
+			delay: 3000,
+		},
+		effect: 'fade',
+  	fadeEffect: {
+    	crossFade: true
+  	},
+	});
+
+	const swiper2 = new Swiper('.partners-wrap', {
+		speed: 1000,
+		autoplay: {
+			delay: 1000,
+		},
+		slidesPerView: 6,
+		loop: true,
+		slidesPerGroup: 2
+	});
+
+  const swiper3 = new Swiper('.slider', {
+		speed: 1000,
+		autoplay: {
+			delay: 3000,
+		},
+		slidesPerView: 1,
+		loop: true,
+    spaceBetween: 100
+	});
+}
